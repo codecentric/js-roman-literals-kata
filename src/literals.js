145768a525dead1d3135c15756ea61bb7b00fafa
Alexander Backes <alexander.backes@codecentric.de>
2024-06-04 14:47:01 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/literals.js

--- src/literals.js
+++ src/literals.js
@@ -4,15 +4,34 @@ export const sum = (a, b) => {
 
 export const toRomanLiteral = (input) => {
   let result = '';
-  const modulo10 = input % 10;
-  if (input >= 10) {
-    result = 'X';
+  const modulo100 = input % 100;
+  if (input >= 100) {
+    result = 'C';
   }
 
-  return result + restAfter10(modulo10);
+  return result + lessThan100(modulo100);
 };
 
-const restAfter10 = (input) => {
+const lessThan100 = (input) => {
+  if (input === 40) {
+    return 'XL';
+  }
+  if (input === 90) {
+    return 'XC';
+  }
+  let result = '';
+
+  if (input >= 50) {
+    result = 'L';
+  }
+  for (let index = 0; index < input % 50; index += 10) {
+    result += 'X';
+  }
+  const modulo10 = input % 10;
+  return result + lessThan10(modulo10);
+};
+
+const lessThan10 = (input) => {
   if (input === 4) {
     return 'IV';
   }
