7ec994ceacd56930f46967903ec032c9d2b58455
Alexander Backes <alexander.backes@codecentric.de>
2024-06-04 13:57:26 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/literals.spec.js

--- src/literals.spec.js
+++ src/literals.spec.js
@@ -1,7 +1,29 @@
-import { sum } from './literals';
+import { sum, toRomanLiteral } from './literals';
 
 describe('Literals', () => {
   it('should add 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+  });
+
+  it('should return I for input 1', () => {
+    // ARRANGE
+    const input = 1;
+
+    // ACT
+    const result = toRomanLiteral(input);
+
+    // ASSERT
+    expect(result).toStrictEqual('I');
+  });
+
+  it('should return II for input 2', () => {
+    // ARRANGE
+    const input = 2;
+
+    // ACT
+    const result = toRomanLiteral(input);
+
+    // ASSERT
+    expect(result).toStrictEqual('II');
   });
 });
