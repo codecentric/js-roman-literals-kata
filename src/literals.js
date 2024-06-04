7e660777df279473ab1db3effe66f5ec6e307e75
Manuel <manuel.wessner@codecentric.de>
2024-06-04 14:51:48 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/literals.js

--- src/literals.js
+++ src/literals.js
@@ -4,32 +4,16 @@ export const sum = (a, b) => {
 
 export const toRomanLiteral = (input) => {
   let result = '';
-  const modulo100 = input % 100;
-  if (input >= 100) {
-    result = 'C';
-  }
-
-  return result + lessThan100(modulo100);
-};
-
-const lessThan100 = (input) => {
-  if (input === 40) {
-    return 'XL';
-  }
-  if (input === 90) {
-    return 'XC';
-  }
-  let result = '';
-
-  if (input >= 50) {
-    result = 'L';
-  }
-  for (let index = 0; index < input % 50; index += 10) {
-    result += 'X';
-  }
   const modulo10 = input % 10;
+
+  const div10 = Math.floor(input / 10)
+  for (const index = 0; index < div10; index++) {
+    result += "X"
+  }
+
   return result + lessThan10(modulo10);
 };
+
 
 const lessThan10 = (input) => {
   if (input === 4) {
